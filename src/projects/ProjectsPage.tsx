71071e41100e2df701c608811fbb33ded4ec34f3
Philip <philipmjobe@gmail.com>
2023-02-22 14:27:02 -0500

    renders props through projectspage.tsx

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import {MOCK_PROJECTS} from './MockProjects'
+import ProjectList from "./ProjectList";
 
 function ProjectsPage() {
   return (
     <>
      <h1>Projects</h1>
-     <pre>{JSON.stringify(MOCK_PROJECTS, null, '')}</pre>
+     <ProjectList projects={MOCK_PROJECTS} />
     </>
-  )
+  );
 }
 
 export default ProjectsPage
