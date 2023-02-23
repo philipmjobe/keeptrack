de8cb82be185aada3d22ba448066286b197af8b2
Philip <philipmjobe@gmail.com>
2023-02-23 16:00:53 -0500

    starts adding api call data

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,11 +1,37 @@
-import React, {useState}from "react";
-import {MOCK_PROJECTS} from './MockProjects'
+import React, {useState, useEffect}from "react";
+import { projectAPI } from "./projectAPI";
 import ProjectList from "./ProjectList";
 import { Project } from "./Project";
 
 
+
 function ProjectsPage() {
-  const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS)
+  const [projects, setProjects] = useState<Project[]>([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<string | undefined>(undefined)
+  const [currentPage, setCurrentPage] = useState(1)
+  useEffect(() => {
+      async function loadProjects() {
+        setLoading(true);
+        try {
+          const data = await projectAPI.get(currentPage);
+          setError('');
+        
+        if (currentPage === 1) {
+          setProjects(data)
+        }else {
+          setProjects((projects) => [...projects, ...data])
+        }
+       } catch (e) {
+          if (e instanceof Error) {
+            setError(e.message);
+          }
+          } finally {
+          setLoading(false);
+        }
+      }
+      loadProjects();
+    }, [currentPage]);
   const saveProject = (project: Project) => {
     let updatedProjects = projects.map((p: Project) => {
       return p.id === project.id ? project : p
@@ -15,9 +41,28 @@ function ProjectsPage() {
   return (
     <>
      <h1>Projects</h1>
+     {error && (
+        <div className="row">
+          <div className="card large error">
+            <section>
+              <p>
+                <span className="icon-alert inverse "></span>
+                {error}
+              </p>
+            </section>
+          </div>
+        </div>
+      )}
      <ProjectList
      onSave={saveProject}
      projects={projects} />
+
+     {loading && (
+      <div className="center-page">
+        <span className="spinner primary"></span>
+        <p>Loading...</p>
+      </div>
+     )}
     </>
   );
 }
